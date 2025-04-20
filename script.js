1ebfde456ef764049fe10e075666e4a19e53bffd
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-19 22:58:21 -0400

    Added a Gameboard object that contains the gameboard and a place marker function. Also added two player objects that contain the name of the player and their markers.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("hello world")
+console.log("hello world");
+
+const Gameboard = {
+  gameboard: [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+  ],
+  placeMarker: function(row, col) {
+    if (this.gameboard[row][col] === "-") {
+        this.gameboard[row][col] = playerOne.marker;
+    }
+  }
+};
+
+const playerOne = {
+    name: "player one",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "player two",
+    marker: "O"
+};
+
+
+
+
+
