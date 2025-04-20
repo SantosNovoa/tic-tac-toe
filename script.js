b683ffd37839a738606cd67514119f5904ad2883
SantosNovoa <novoasj8729@student.laccd.edu>
2025-04-20 00:43:42 -0400

    removed placeMarker being two functions for each player. Instead turned it into a single function that passes the player as a argument

--- script.js
+++ script.js
@@ -11,18 +11,9 @@ const Gameboard = {
       console.log(this.gameboard[i].join("|"));
     }
   },
-  placeMarkerPlayerOne: function (row, col) {
+  placeMarker: function (row, col, player) {
     if (this.gameboard[row][col] === "-") {
-      this.gameboard[row][col] = playerOne.marker;
-      console.log("\n");
-      Gameboard.displayGameboard();
-    } else {
-      console.log("Error, spot filled.");
-    }
-  },
-  placeMarkerPlayerTwo: function (row, col) {
-    if (this.gameboard[row][col] === "-") {
-      this.gameboard[row][col] = playerTwo.marker;
+      this.gameboard[row][col] = player.marker;
       console.log("\n");
       Gameboard.displayGameboard();
     } else {
@@ -53,24 +44,30 @@ const Gameboard = {
       ) {
         console.log(`${playerOne.name} won by column.`);
       } else if (
-        this.gameboard[0][i] === playerOne.marker &&
-        this.gameboard[1][i] === playerOne.marker &&
-        this.gameboard[2][i] === playerOne.marker
+        this.gameboard[0][i] === playerTwo.marker &&
+        this.gameboard[1][i] === playerTwo.marker &&
+        this.gameboard[2][i] === playerTwo.marker
       ) {
         console.log(`${playerTwo.name} won by column.`);
       }
     }
     //checking diagonal
     if (
-      this.gameboard[0][0] === playerOne.marker &&
-      this.gameboard[1][1] === playerOne.marker &&
-      this.gameboard[2][2] === playerOne.marker
+      (this.gameboard[0][0] === playerOne.marker &&
+        this.gameboard[1][1] === playerOne.marker &&
+        this.gameboard[2][2] === playerOne.marker) ||
+      (this.gameboard[0][2] === playerOne.marker &&
+        this.gameboard[1][1] === playerOne.marker &&
+        this.gameboard[2][0] === playerOne.marker)
     ) {
       console.log(`${playerOne.name} won by diagonal.`);
     } else if (
-      this.gameboard[0][0] === playerTwo.marker &&
-      this.gameboard[1][1] === playerTwo.marker &&
-      this.gameboard[2][2] === playerTwo.marker
+      (this.gameboard[0][0] === playerTwo.marker &&
+        this.gameboard[1][1] === playerTwo.marker &&
+        this.gameboard[2][2] === playerTwo.marker) ||
+      (this.gameboard[0][2] === playerTwo.marker &&
+        this.gameboard[1][1] === playerTwo.marker &&
+        this.gameboard[2][0] === playerTwo.marker)
     ) {
       console.log(`${playerTwo.name} won by diagonal.`);
     }
@@ -87,8 +84,14 @@ const playerTwo = {
   marker: "O",
 };
 
-Gameboard.placeMarkerPlayerTwo(1, 0);
-Gameboard.placeMarkerPlayerTwo(1, 1);
-Gameboard.placeMarkerPlayerTwo(2, 2);
-Gameboard.placeMarkerPlayerTwo(1, 2);
+// Gameboard.placeMarkerPlayerTwo(1, 0);
+// Gameboard.placeMarkerPlayerTwo(1, 1);
+// Gameboard.placeMarkerPlayerTwo(2, 2);
+// Gameboard.placeMarkerPlayerTwo(1, 2);
+
+Gameboard.placeMarker(0, 1, playerOne);
+Gameboard.placeMarker(0, 2, playerTwo);
+Gameboard.placeMarker(1, 1, playerTwo);
+Gameboard.placeMarker(2, 0, playerTwo);
+
 Gameboard.determineWinner();
